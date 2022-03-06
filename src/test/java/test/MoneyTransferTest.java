891b52fccba26573fc7b2e8eec0254ff9ae0b555
test <test>
2022-03-06 13:53:55 +0300

    update tests

---
+++ src/test/java/test/MoneyTransferTest.java
@@ -0,0 +1,45 @@
+package test;
+
+import data.DataHelper;
+import lombok.val;
+import org.junit.jupiter.api.Test;
+import page.LoginPageV1;
+import page.LoginPageV2;
+import page.LoginPageV3;
+import page.VerificationPage;
+
+import static com.codeborne.selenide.Selenide.open;
+
+class MoneyTransferTest {
+    @Test
+    void shouldTransferMoneyBetweenOwnCardsV1() {
+      open("http://localhost:9999");
+      val loginPage = new LoginPageV1();
+//    val loginPage = open("http://localhost:9999", LoginPageV1.class);
+      val authInfo = DataHelper.getAuthInfo();
+      val verificationPage = loginPage.validLogin(authInfo);
+      val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+      verificationPage.validVerify(verificationCode);
+    }
+
+  @Test
+  void shouldTransferMoneyBetweenOwnCardsV2() {
+    open("http://localhost:9999");
+    val loginPage = new LoginPageV2();
+//    val loginPage = open("http://localhost:9999", LoginPageV2.class);
+    val authInfo = DataHelper.getAuthInfo();
+    val verificationPage = loginPage.validLogin(authInfo);
+    val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+    verificationPage.validVerify(verificationCode);
+  }
+
+  @Test
+  void shouldTransferMoneyBetweenOwnCardsV3() {
+    val loginPage = open("http://localhost:9999", LoginPageV3.class);
+    val authInfo = DataHelper.getAuthInfo();
+    val verificationPage = loginPage.validLogin(authInfo);
+    val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+    verificationPage.validVerify(verificationCode);
+  }
+}
+
