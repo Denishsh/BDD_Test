1976e006beda3685c25d8441ca60d4c0fbf9b14e
test <test>
2022-03-08 16:44:13 +0300

    update tests

--- src/test/java/page/DashboardPage.java
+++ src/test/java/page/DashboardPage.java
@@ -1,14 +1,62 @@
 package page;
 
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
+import lombok.val;
 
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.*;
 
 public class DashboardPage {
-  private SelenideElement heading = $("[data-test-id=dashboard]");
+    private SelenideElement heading = $("[data-test-id=dashboard]");
+    private ElementsCollection cards = $$(".list__item");
+    private final String balanceStart = "баланс: ";
+    private final String balanceFinish = " р.";
 
-  public DashboardPage() {
-    heading.shouldBe(visible);
-  }
+    private SelenideElement depositButton = $("[data-test-id=action-deposit]");
+    private SelenideElement transferButton = $("[data-test-id=action-transfer]");
+    private SelenideElement amountField = $("[data-test-id=amount] input");
+    private SelenideElement fromField = $("[data-test-id=from] input");
+
+    public DashboardPage() {
+        heading.shouldBe(visible);
+        depositButton.shouldBe(visible);
+    }
+
+    public int getFirstCardBalance() {
+        val text = cards.first().text();
+        return extractBalance(text);
+    }
+
+    public int getCardBalance(int order) {
+        val text = cards.get(order).text();
+        return extractBalance(text);
+    }
+
+    public void transferToCard(int amount) {
+        depositButton.click();
+        amountField.setValue(String.valueOf(amount));
+        fromField.setValue("5559 0000 0000 0002");
+        transferButton.click();
+    }
+
+    public void transferToCard(int amount, int card) {
+        sleep(2000);
+        if (card == 1) {
+            $$("[data-test-id=action-deposit]").last().click();
+        } else {
+            depositButton.click();
+        }
+        amountField.setValue(String.valueOf(amount));
+        fromField.setValue("5559 0000 0000 000" + card);
+        transferButton.click();
+        sleep(2000);
+    }
+
+    private int extractBalance(String text) {
+        val start = text.indexOf(balanceStart);
+        val finish = text.indexOf(balanceFinish);
+        val value = text.substring(start + balanceStart.length(), finish);
+        return Integer.parseInt(value);
+    }
 }
--- src/test/java/test/MoneyTransferTest.java
+++ src/test/java/test/MoneyTransferTest.java
@@ -6,40 +6,86 @@ import org.junit.jupiter.api.Test;
 import page.LoginPageV1;
 import page.LoginPageV2;
 import page.LoginPageV3;
-import page.VerificationPage;
 
 import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MoneyTransferTest {
     @Test
     void shouldTransferMoneyBetweenOwnCardsV1() {
-      open("http://localhost:9999");
-      val loginPage = new LoginPageV1();
+        open("http://localhost:9999");
+        val loginPage = new LoginPageV1();
 //    val loginPage = open("http://localhost:9999", LoginPageV1.class);
-      val authInfo = DataHelper.getAuthInfo();
-      val verificationPage = loginPage.validLogin(authInfo);
-      val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-      verificationPage.validVerify(verificationCode);
+        val authInfo = DataHelper.getAuthInfo();
+        val verificationPage = loginPage.validLogin(authInfo);
+        val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        verificationPage.validVerify(verificationCode);
     }
 
-  @Test
-  void shouldTransferMoneyBetweenOwnCardsV2() {
-    open("http://localhost:9999");
-    val loginPage = new LoginPageV2();
+    @Test
+    void shouldTransferMoneyBetweenOwnCardsV2() {
+        open("http://localhost:9999");
+        val loginPage = new LoginPageV2();
 //    val loginPage = open("http://localhost:9999", LoginPageV2.class);
-    val authInfo = DataHelper.getAuthInfo();
-    val verificationPage = loginPage.validLogin(authInfo);
-    val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-    verificationPage.validVerify(verificationCode);
-  }
+        val authInfo = DataHelper.getAuthInfo();
+        val verificationPage = loginPage.validLogin(authInfo);
+        val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        verificationPage.validVerify(verificationCode);
+    }
 
-  @Test
-  void shouldTransferMoneyBetweenOwnCardsV3() {
-    val loginPage = open("http://localhost:9999", LoginPageV3.class);
-    val authInfo = DataHelper.getAuthInfo();
-    val verificationPage = loginPage.validLogin(authInfo);
-    val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-    verificationPage.validVerify(verificationCode);
-  }
+    @Test
+    void shouldTransferMoneyBetweenOwnCardsV3() {
+        val loginPage = open("http://localhost:9999", LoginPageV3.class);
+        val authInfo = DataHelper.getAuthInfo();
+        val verificationPage = loginPage.validLogin(authInfo);
+        val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        verificationPage.validVerify(verificationCode);
+    }
+
+    @Test
+    void checkBalanceTest() {
+        open("http://localhost:9999");
+        val loginPage = new LoginPageV1();
+        val authInfo = DataHelper.getAuthInfo();
+        val verificationPage = loginPage.validLogin(authInfo);
+        val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        val dashboard = verificationPage.validVerify(verificationCode);
+        val balance1 = dashboard.getFirstCardBalance();
+        System.out.println(balance1);
+        val balance2 = dashboard.getCardBalance(1);
+        System.out.println(balance2);
+    }
+
+    @Test
+    void transferTest() {
+        open("http://localhost:9999");
+        val loginPage = new LoginPageV1();
+        val authInfo = DataHelper.getAuthInfo();
+        val verificationPage = loginPage.validLogin(authInfo);
+        val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        val dashboard = verificationPage.validVerify(verificationCode);
+        val balance1 = dashboard.getFirstCardBalance();
+        val balance2 = dashboard.getCardBalance(1);
+        val amount = 1000;
+        dashboard.transferToCard(amount);
+        assertEquals(balance1 + amount, dashboard.getFirstCardBalance());
+        assertEquals(balance2 - amount, dashboard.getCardBalance(1));
+    }
+
+    @Test
+    void transferSecondTest() {
+        open("http://localhost:9999");
+        val loginPage = new LoginPageV1();
+        val authInfo = DataHelper.getAuthInfo();
+        val verificationPage = loginPage.validLogin(authInfo);
+        val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        val dashboard = verificationPage.validVerify(verificationCode);
+        val balance1 = dashboard.getFirstCardBalance();
+        val balance2 = dashboard.getCardBalance(1);
+        val amount = 1000;
+        dashboard.transferToCard(amount, 1);
+        assertEquals(balance2 + amount, dashboard.getCardBalance(1));
+        assertEquals(balance1 - amount, dashboard.getFirstCardBalance());
+    }
 }
 
